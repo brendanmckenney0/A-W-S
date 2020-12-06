c018d2493e8a27646f0605cbe9f12f5ee1182cd6
Brendan McKenney <brendanmckenney@gmail.com>
2020-12-06 14:33:50 -0500

    update readme

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ from termcolor import colored
 banner = pyfiglet.figlet_format("C-W-S")
 url_hist = []
 readme = """
-Hello! I created this program for my CSI-160 Python programming course.
-This is my final project for the course.
-This web scraper is designed to work with any website that allows for web scraping.
-As long as the user can find the Class name and class type they want to retrieve,
-they can do so by entering it.
+Hello! I created this program for my CSI-160 Python programming course. 
+This is my final project for the course. 
 
-This program gives you the option to save
+There are two main functions of this program:
+1. To retrieve the html content of a website using just it's URL.
+2. To do a targeted scrape by using the websites url, the class type in the html that the user wants to scrape, and the class name.
+
+This program gives you the option to save URL's you enter into the scraper to be saved to a URL history list.
+These are only saved for your viewing during your current session, locally. They aren't saved anywhere else. 
+The program also gives the option whether to save URL's or not into URL history. 
+
+In addition to URL's, data pulled from websites is not saved in any way. 
 
 """
 
